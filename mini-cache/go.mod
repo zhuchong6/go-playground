e768b73316e933fea0349ca1ffd8c1be805af007
zhuchong <1043199504@qq.com>
2024-01-11 17:50:56 +0800

    add mini-cache project and support lru algorithm

---
+++ mini-cache/go.mod
@@ -0,0 +1,3 @@
+module zhuchong.com/mini-cache
+
+go 1.21.4
---
+++ mini-cache/mcache/lru/lru.go
@@ -0,0 +1,81 @@
+package lru
+
+import "container/list"
+
+// Cache is a LRU cache ,not safe for concurrent access
+type Cache struct {
+	maxBytes int64
+	nBytes   int64
+	ll       *list.List // a double linked list
+	cache    map[string]*list.Element
+	// optional and executed when an entry is purged.
+	OnEvicted func(key string, value Value)
+}
+
+func (c *Cache) Len() int{
+	return c.ll.Len()
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+// Value use Len to count how many bytes it takes
+type Value interface {
+	Len() int
+}
+
+// Cache's constructor
+func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+// RemoveOldest removes the oldest item
+func (c *Cache) RemoveOldest(){
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nBytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+// Add adds a value to the cache
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nBytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nBytes += int64(len(key)) + int64(value.Len())
+	}
+
+	for c.maxBytes !=0 && c.maxBytes < c.nBytes {
+		c.RemoveOldest()
+	}
+}
+
+// get key
+// 1. find node from map
+// 2. move node to the top of the double linked list
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
